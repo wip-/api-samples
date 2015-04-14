90384ad41e9eedd613370d35f86fa9e894ca76dd
wip <wip-@users.noreply.github.com>
2015-04-14 13:51:51 +0900

    Add the sample code from https://developers.google.com/api-client-library/dotnet/get_started (need to clean nuGet setup)

---
+++ dotnet/SimpleAPIExample/Program.cs
@@ -0,0 +1,61 @@
+﻿// This is a copy of the sample code found at https://developers.google.com/api-client-library/dotnet/get_started
+
+using System;
+using System.Threading.Tasks;
+
+using Google.Apis.Discovery.v1;
+using Google.Apis.Discovery.v1.Data;
+using Google.Apis.Services;
+
+namespace Discovery.ListAPIs
+{
+    /// <summary>
+    /// This example uses the discovery API to list all APIs in the discovery repository.
+    /// https://developers.google.com/discovery/v1/using.
+    /// <summary>
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Discovery API Sample");
+            Console.WriteLine("====================");
+            try
+            {
+                new Program().Run().Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("ERROR: " + e.Message);
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private async Task Run()
+        {
+            // Create the service.
+            var service = new DiscoveryService(new BaseClientService.Initializer
+            {
+                ApplicationName = "Discovery Sample",
+                ApiKey = "[YOUR_API_KEY_HERE]",
+            });
+
+            // Run the request.
+            Console.WriteLine("Executing a list request...");
+            var result = await service.Apis.List().ExecuteAsync();
+
+            // Display the results.
+            if (result.Items != null)
+            {
+                foreach (DirectoryList.ItemsData api in result.Items)
+                {
+                    Console.WriteLine(api.Id + " - " + api.Title);
+                }
+            }
+        }
+    }
+}
